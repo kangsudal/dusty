a6f87bf05e9734b223f046c0a4f207dbe4078d85
kangsudal_notebook <kangsudal@gmail.com>
2022-12-05 16:44:50 +0900

    Dio로 데이터 요청하기&데이터모델링

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,11 +1,46 @@
+import 'package:dio/dio.dart';
 import 'package:dusty/component/category_card.dart';
 import 'package:dusty/component/main_app_bar.dart';
 import 'package:dusty/component/main_drawer.dart';
 import 'package:dusty/const/colors.dart';
+import 'package:dusty/model/stat_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    fetchData();
+    super.initState();
+  }
+
+  fetchData() async {
+    final response = await Dio().get(
+        'http://apis.data.go.kr/B552584/ArpltnStatsSvc/getCtprvnMesureLIst',
+        queryParameters: {
+          'serviceKey': dotenv.env['serviceKey'],
+          'returnType': 'json',
+          'numOfRows': 30,
+          'pageNo': 1,
+          'itemCode': 'PM10',
+          'dataGubun': 'HOUR',
+          'searchCondition': 'WEEK',
+        });
+
+    print(
+      //json -> StatModel object
+      response.data['response']['body']['items'].map(
+        (item) => StatModel.fromJson(json: item),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
