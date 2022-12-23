70f963798f8edadf00b5d560e5def7e66e445df4
kangsudal_notebook <kangsudal@gmail.com>
2022-12-23 16:50:53 +0900

    Bad State: No Element 버그 수정 & 마무리

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -29,53 +29,66 @@ class _HomeScreenState extends State<HomeScreen> {
   ScrollController scrollController = ScrollController();
 
   Future<void> fetchData() async {
-    final now = DateTime.now();
-    final fetchTime = DateTime(
-      now.year,
-      now.month,
-      now.day,
-      now.hour,
-    );
+    try {
+      final now = DateTime.now();
+      final fetchTime = DateTime(
+        now.year,
+        now.month,
+        now.day,
+        now.hour,
+      );
 
-    final box = Hive.box<StatModel>(ItemCode.PM10.name);
-    final recent = box.values.last as StatModel;
-    //최근데이터시간과 동일하다면 밑에 코드들을 실행할 필요가 없다.
+      final box = Hive.box<StatModel>(ItemCode.PM10.name);
 
-    if (fetchTime.isAtSameMomentAs(recent.dateTime)) {
-      //isSameMomentAs:시간 비교
-      print('이미 최신데이터가 있습니다.');
-      return;
-    }
+      if(box.isNotEmpty){
+        final recent = box.values.last as StatModel;
+        //최근데이터시간과 동일하다면 밑에 코드들을 실행할 필요가 없다.
 
-    //List: 시간별, StatModel: 각 지역별 데이터 모아놓은 객체
-    // Map<ItemCode, List<StatModel>> stats = {};
-    List<Future<List<StatModel>>> futures = [];
-    for (ItemCode itemCode in ItemCode.values) {
-      final Future<List<StatModel>> response =
-          StatRepository.fetchData(itemCode: itemCode);
-      futures.add(response);
-    }
-    final results = await Future.wait(futures); //모든 future들이 다 끝날때까지 기다린다.
-
-    Map<ItemCode, List<StatModel>> statsByItemCode;
-    for (int i = 0; i < results.length; i++) {
-      final key = ItemCode.values[i]; //ItemCode
-      final value = results[i]; //List<StatModel>
-
-      //Hive에 데이터를 저장해준다.
-      final box = Hive.box<StatModel>(key.name);
-      for (StatModel stat in value) {
-        box.put(stat.dateTime.toString(), stat);
+        print('${recent.dateTime} ($fetchTime)');
+        if (recent.dateTime.isAtSameMomentAs(fetchTime)) {
+          //isSameMomentAs:시간 비교
+          print('이미 최신데이터가 있습니다.');
+          return;
+        }
       }
-      final allKeys = box.keys.toList();
-      if (allKeys.length > 24) {
-        //모든 데이터의 개수가 24개 이상일때
-        //처음~24개까지 데이터만 남기고 지워준다
-        final deleteKeys = allKeys.sublist(0, allKeys.length - 24);
-        //제일 과거 데이터부터~최근24개빼고 담는다
 
-        box.deleteAll(deleteKeys);
+
+      //List: 시간별, StatModel: 각 지역별 데이터 모아놓은 객체
+      // Map<ItemCode, List<StatModel>> stats = {};
+      List<Future<List<StatModel>>> futures = [];
+      for (ItemCode itemCode in ItemCode.values) {
+        final Future<List<StatModel>> response =
+            StatRepository.fetchData(itemCode: itemCode);
+        futures.add(response);
       }
+      final results = await Future.wait(futures); //모든 future들이 다 끝날때까지 기다린다.
+
+      Map<ItemCode, List<StatModel>> statsByItemCode;
+      for (int i = 0; i < results.length; i++) {
+        final key = ItemCode.values[i]; //ItemCode
+        final value = results[i]; //List<StatModel>
+
+        //Hive에 데이터를 저장해준다.
+        final box = Hive.box<StatModel>(key.name);
+        for (StatModel stat in value) {
+          box.put(stat.dateTime.toString(), stat);
+        }
+        final allKeys = box.keys.toList();
+        if (allKeys.length > 24) {
+          //모든 데이터의 개수가 24개 이상일때
+          //처음~24개까지 데이터만 남기고 지워준다
+          final deleteKeys = allKeys.sublist(0, allKeys.length - 24);
+          //제일 과거 데이터부터~최근24개빼고 담는다
+
+          box.deleteAll(deleteKeys);
+        }
+      }
+    } on DioError catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('인터넷 연결이 원활하지 않습니다.'),
+        ),
+      );
     }
   }
 
@@ -137,44 +150,49 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           body: Container(
             color: status.primaryColor,
-            child: CustomScrollView(
-              controller: scrollController,
-              slivers: [
-                MainAppBar(
-                  region: region,
-                  stat: recentStat,
-                  status: status,
-                  dateTime: recentStat.dateTime,
-                  isExpanded: isExpanded,
-                ),
-                SliverToBoxAdapter(
-                  //Sliver가 아닌 위젯을 넣을 수 있게해준다
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      CategoryCard(
-                        region: region,
-                        darkColor: status.darkColor,
-                        lightColor: status.lightColor,
-                      ), //종류별 통계
-                      SizedBox(height: 16),
-                      ...ItemCode.values.map((itemCode) {
-                        return Padding(
-                          padding: const EdgeInsets.only(bottom: 16.0),
-                          child: HourlyCard(
-                            darkColor: status.darkColor,
-                            lightColor: status.lightColor,
-                            itemCode: itemCode,
-                            region: region,
-                          ),
-                        );
-                      }).toList(),
-                      //시간별 미세먼지
-                      SizedBox(height: 16),
-                    ],
+            child: RefreshIndicator(
+              onRefresh: () async {
+                await fetchData();
+              },
+              child: CustomScrollView(
+                controller: scrollController,
+                slivers: [
+                  MainAppBar(
+                    region: region,
+                    stat: recentStat,
+                    status: status,
+                    dateTime: recentStat.dateTime,
+                    isExpanded: isExpanded,
                   ),
-                ),
-              ],
+                  SliverToBoxAdapter(
+                    //Sliver가 아닌 위젯을 넣을 수 있게해준다
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        CategoryCard(
+                          region: region,
+                          darkColor: status.darkColor,
+                          lightColor: status.lightColor,
+                        ), //종류별 통계
+                        SizedBox(height: 16),
+                        ...ItemCode.values.map((itemCode) {
+                          return Padding(
+                            padding: const EdgeInsets.only(bottom: 16.0),
+                            child: HourlyCard(
+                              darkColor: status.darkColor,
+                              lightColor: status.lightColor,
+                              itemCode: itemCode,
+                              region: region,
+                            ),
+                          );
+                        }).toList(),
+                        //시간별 미세먼지
+                        SizedBox(height: 16),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         );
